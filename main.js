5052968d1a8c5172e09dba775d353d853560087b
Brittany Moore <britkamo@gmail.com>
2018-04-04 19:52:07 -0500

    Add prompts for user input and if statements to check the value of rating input by the user is within correct parameters. Add function to add the user input rating to the rating array and function to calculate an rounded average rating.

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // console.log('This file is linked!')
 var teacherFullName = 'Sally Jones';
 var departmentName = 'Physics';
-var rating1 = 3.4;
-var rating2 = 5;
-var rating3 = 4.2;
-var averageRating = (rating1 + rating2 + rating3) / 3;
-var roundedRating = Math.round(averageRating * 10) / 10;
+var ratingsArray = [3.5, 5, 4.2, 5, 1, 1, 1, 1];
+// var rating1 = 3.4;
+// var rating2 = 5;
+// var rating3 = 4.2;
+var averageRating; //= (rating1 + rating2 + rating3) / 3;
+var roundedRating; //= Math.round(averageRating * 10) / 10;
 var teacherCourse1 = 'Biology';
 var teacherCourse2 = 'Physics';
 
@@ -18,13 +19,50 @@ var studentCourse2 = 'Econ 101';
 
 var semester = 'Fall 2018';
 
-console.log('Teacher: ' + teacherFullName);
-console.log('Department: ' + departmentName);
-console.log('Ratings: ' + rating1 + ', ' + rating2 + ', ' + rating3);
-console.log('Average Rating: ' + roundedRating);
-console.log('Student: ' + studentFullName);
-console.log('Major: ' + studentMajor);
-console.log('Email: ' + studentEmail);
-console.log('GPA: ' + studentGpa);
-console.log('Courses: ' + studentCourse1 + ', ' + studentCourse2);
-console.log('Semester: ' + semester);
+var userRating = prompt("We would like for you to review . Please enter a rating between 0.0 - 5.0.", "0-5");
+
+// I think this could be a function or loop somehow...
+if(userRating == null) {
+  alert("You can always leave a review later!");
+} else if(userRating >= 0 && userRating <= 5) {
+  addTeacherRating(ratingsArray, userRating);
+  //averageStuff(ratingsArray);
+  alert("Thanks for your review! <teacher's name> average rating is now: " + averageRating);
+} else {
+  userRating = prompt("We won't ask again but would really appreciate your review . Please make sure your rating is between 0.0 - 5.0.", "0-5");
+  if(userRating == null) {
+    alert("You can always leave a review later!");
+  } else if(userRating >= 0 && userRating <= 5) {
+    addTeacherRating(ratingsArray, userRating);
+    alert("Thanks for your review! <teacher's name> average rating is now: " + averageRating);
+  } else {
+    alert("You can always leave a review later!");
+  }
+}
+// Add a new rating entered bye the user to the passed array
+function addTeacherRating(existingArray, newRating) {
+  existingArray.push(newRating);
+  return existingArray;
+}
+// take some numbers and find the average
+function averageStuff(existingArray) {
+  //var averageRating; //= (rating1 + rating2 + rating3) / 3;
+  var sumOfNumbers = 0;
+  var arrayTotal = existingArray.length;
+  for( i=0; i < arrayTotal; i++) {
+    sumOfNumbers += existingArray[i];
+  }
+  averageRating = sumOfNumbers / arrayTotal;
+  roundedRating = Math.round(averageRating * 10) / 10;
+  return roundedRating;
+}
+// console.log('Teacher: ' + teacherFullName);
+// console.log('Department: ' + departmentName);
+// console.log('Ratings: ' + rating1 + ', ' + rating2 + ', ' + rating3);
+// console.log('Average Rating: ' + roundedRating);
+// console.log('Student: ' + studentFullName);
+// console.log('Major: ' + studentMajor);
+// console.log('Email: ' + studentEmail);
+// console.log('GPA: ' + studentGpa);
+// console.log('Courses: ' + studentCourse1 + ', ' + studentCourse2);
+// console.log('Semester: ' + semester);
